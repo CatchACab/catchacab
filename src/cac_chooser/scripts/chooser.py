17a2e0da4d5a007ee1fbb669817a6026e502b8a0
SamuelValenzuela <38457628+SamuelValenzuela@users.noreply.github.com>
2018-11-17 21:42:23 +0100

    adds evaluate_ride_request

--- src/cac_chooser/scripts/chooser.py
+++ src/cac_chooser/scripts/chooser.py
@@ -15,6 +15,14 @@ def detect():
         rate.sleep()
         counter += 1
 
+blacklist_badness = {"bottle": 1}  # blacklist dictionary: "item_name": badness
+
+def evaluate_ride_request(hand_probability, blacklist_items):  # item in items: (item_name, item_probability)
+    blacklist_sum = 0
+    for item in blacklist_items:
+        blacklist_sum += blacklist_badness[item[0]] * item[1]
+    return hand_probability - blacklist_sum >= 0.5
+        
 if __name__ == '__main__':
     try:
         detect()
